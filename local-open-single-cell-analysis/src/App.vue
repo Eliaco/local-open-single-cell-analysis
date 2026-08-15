95ea261a06bbcd7051a2ba1a6e6444ea291630d2
eliaco <elia.covino.a@gmail.com>
2026-08-15 20:45:54 +0200

    feat: New Vue.js project

---
+++ local-open-single-cell-analysis/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TestWASM from "./components/TestWASM.vue";
+</script>
+
+<template>
+  <header>local open single cell analysis</header>
+  <main>
+    <!-- TODO -->
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
